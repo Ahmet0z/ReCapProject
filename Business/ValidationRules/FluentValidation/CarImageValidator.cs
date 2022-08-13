52fb4dd081d89775d1d8bb4e48f6fbacacc2a4b3
Ahmet0z <l9120016@std.yildiz.edu.tr>
2022-08-13 19:18:36 +0300

    Duplicated Rule Removed

--- Business/ValidationRules/FluentValidation/CarImageValidator.cs
+++ Business/ValidationRules/FluentValidation/CarImageValidator.cs
@@ -8,7 +8,6 @@ namespace Business.ValidationRules.FluentValidation
         public CarImageValidator()
         {
             RuleFor(c => c.CarId).NotEmpty();
-            RuleFor(c => c.CarId).NotEmpty();
             RuleFor(c => c.CarId).GreaterThan(0);
         }
     }
