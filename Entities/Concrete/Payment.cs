2c785c59ae57e71bb05940a78ce5c9f257b2af31
Ahmet0z <l9120016@std.yildiz.edu.tr>
2022-10-16 14:21:04 +0300

    DB Change Applied

--- Entities/Concrete/Payment.cs
+++ Entities/Concrete/Payment.cs
@@ -5,7 +5,7 @@ namespace Entities.Concrete
     public class Payment : IEntity
     {
         public int PaymentId { get; set; }
-        public int CustomerId { get; set; }
+        public int UserId { get; set; }
         public string CreditCardNumber { get; set; }
         public decimal Price { get; set; }
         public string ExpirationDate { get; set; }
